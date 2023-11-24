04936f66a2e8deb5ebc8dfbdd3729225d8424b27
ayse ozceylan <abdullahozbsr@gmail.com>
2023-11-24 18:42:31 +0200

    change made

--- src/odev5.java
+++ src/odev5.java
@@ -5,12 +5,13 @@ public class odev5 {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Boyunuzu giriniz(metre cinsinden) : ");
-        kilo = input.nextDouble();
-
-        System.out.print("Kilonuzu giriniz(kg cinsinden) : ");
         height = input.nextDouble();
 
-        double indeks = kilo / height * height;
+        System.out.print("Kilonuzu giriniz(kg cinsinden) : ");
+        kilo = input.nextDouble();
+
+        double indeks = kilo /
+                (height * height);
         System.out.println("Vücut kitle indeksiniz : " + indeks);
 
 
